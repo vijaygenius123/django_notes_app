50a8eba4f464a4ca2b6f8e20240648ab2601ed82
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-04 13:18:32 +0530

    Added Functionality To Save Form Data

--- notes/views.py
+++ notes/views.py
@@ -8,4 +8,10 @@ def list(request):
     return render(request,'notes/list.html')
 
 def create(request):
-     return render(request,'notes/create.html')
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return render(request,'notes/list.html')
+    form = NoteForm()
+    return render(request,'notes/create.html',{'form':form})
