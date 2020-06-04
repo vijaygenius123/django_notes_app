c76716c2fbcd840b4642d9d23c3871056edfa5f8
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-04 12:35:45 +0530

    Created Note Model And Added It In Admin

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=50, blank=True, null=True)
+    content = models.TextField(max_length=500, blank=True, null=True)
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
