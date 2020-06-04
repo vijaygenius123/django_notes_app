726b8c8f2bd81777a2041daa60a85086080d9429
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-06-04 13:22:26 +0530

    Added Redirect Functionality

--- notes/views.py
+++ notes/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
+from .models import Note
 from .forms import NoteForm
 
 # Create your views here.
 def list(request):
-    return render(request,'notes/list.html')
+    notes = Note.objects.all()
+
+    return render(request,'notes/list.html',{'notes':notes})
 
 def create(request):
     if request.method == 'POST':
         form = NoteForm(request.POST)
         if form.is_valid():
             form.save()
-        return render(request,'notes/list.html')
+            return redirect('list')
     form = NoteForm()
     return render(request,'notes/create.html',{'form':form})
